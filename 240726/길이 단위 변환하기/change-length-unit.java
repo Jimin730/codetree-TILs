21d32d2fa1c3f587d87f9fe7653f3b13a0930694
Jimin730 <108002997+Jimin730@users.noreply.github.com>
2024-07-26 19:55:13 +0900

    Title: 길이 단위 변환하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240726/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double ft = 9.2;
+        double mi = 1.3;
+
+        System.out.printf("9.2ft = %.1fcm", (ft*30.48));
+        System.out.println();
+        System.out.printf("1.3mi = %.1fcm", (mi*160934));
+        
+        }
+}
