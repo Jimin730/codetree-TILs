c228dd947cca3393afb7be51cccb14744f426341
Jimin730 <108002997+Jimin730@users.noreply.github.com>
2024-07-26 21:29:53 +0900

    Title: 특정 조건 세 정수 비교, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = Math.min(Math.min(a,b),c);
+        
+        if(a==min){
+            System.out.print(1+" ");
+        } else {
+            System.out.print(0+" ");
+        }
+
+        if(a==b && b==c) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        
+    }
+}
